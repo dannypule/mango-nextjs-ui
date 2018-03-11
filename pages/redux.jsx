bd0ccdb0a62cea285b2fa0d67d2cebea1d1abc24
Danny Pule <kojikusan@gmail.com>
2018-03-11 13:44:29 +0000

    class component for redux

--- pages/redux.jsx
+++ pages/redux.jsx
@@ -1,39 +1,44 @@
+import React, { Component } from 'react'
 import withRedux from 'next-redux-wrapper'
 import Proptypes from 'prop-types'
 import store, { add, minus } from '../store/store'
 
-const Page = ({ number, numberTwo, custom, add, minus }) => (
-  <div>
-    <h1>{number}</h1>
-    <p>Custom: {custom}</p>
-    <div>
-      <button onClick={add}>add</button>
-    </div>
+class Page extends Component {
+  static propTypes = {
+    number: Proptypes.number,
+    numberTwo: Proptypes.number,
+    custom: Proptypes.string
+  }
 
-    <h1>{numberTwo}</h1>
-    <div>
-      <button onClick={minus}>minus</button>
-    </div>
-  </div>
-)
-Page.getInitialProps = ({ store, isServer, pathname, query }) => {
-  // component will read from store's state when rendered
-  store.dispatch({ type: 'ADD', payload: 12 })
+  static getInitialProps = ({ store, isServer, pathname, query }) => {
+    // component will read from store's state when rendered
+    store.dispatch({ type: 'ADD', payload: 12 })
 
-  console.log(isServer)
-  console.log(pathname)
-  console.log(query)
+    console.log(isServer)
+    console.log(pathname)
+    console.log(query)
 
-  // pass some custom props to component from here
-  return { custom: 'yayy!1!' }
-}
+    // pass some custom props to component from here
+    return { custom: 'yayy!1!' }
+  }
 
-Page.propTypes = {
-  number: Proptypes.number.isRequired,
-  numberTwo: Proptypes.number.isRequired,
-  custom: Proptypes.func.isRequired,
-  add: Proptypes.func.isRequired,
-  minus: Proptypes.func.isRequired
+  render() {
+    const { number, custom, numberTwo, add, minus } = this.props
+    return (
+      <div>
+        <h1>{number}</h1>
+        <p>Custom: {custom}</p>
+        <div>
+          <button onClick={add}>add</button>
+        </div>
+
+        <h1>{numberTwo}</h1>
+        <div>
+          <button onClick={minus}>minus</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default withRedux(
